bcc5ae51d84ae131c71f19387525b510c7f4c7ef
hellolyd2012 <hellolyd2012>
2024-10-07 14:28:44 +0800

    修改Kernel/Kernel.c，更新内核启动信息 - 首次提交

--- Kernel/Kernel.c
+++ Kernel/Kernel.c
@@ -7,5 +7,5 @@ void _KernelInit() {
 void _KernelMain(void* info_table) {
     // TODO
     TtySetTheme(VGA_COLOR_GREEN, VGA_COLOR_BLACK);
-    TtyPutStr("Hello kernel world!\nThis is second line.");
+    TtyPutStr("Wecome to RemeoOS!\n");
 }
