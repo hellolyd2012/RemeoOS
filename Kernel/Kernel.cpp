98941992d0f720f176b00e869f22d0ec47aa4884
hellolyd2012 <lyd20122024@outlook.com>
2024-10-20 14:36:58 +0800

    feat: 添加 RemeoOS 内核主程序
    - 新增 KernelMain 函数，作为内核入口点
    - 包含必要的头文件：UEFI、FrameBuffer、Elf
    - 目前内核主循环为空，后续将添加具体功能

---
+++ Kernel/Kernel.cpp
@@ -0,0 +1,15 @@
+// 创建于2024.10.20
+// RemeoOS 内核
+
+#include <UEFI/UEFI.hpp>
+#include <UEFI/FrameBuffer.hpp>
+#include <Exec/Elf.hpp>
+
+extern "C" void KernelMain()
+{
+    while (true)
+    {
+        
+    }
+    
+} 
