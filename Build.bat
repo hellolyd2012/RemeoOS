089c47a9116e9bdef3f8dafd094690c43093c63d
hellolyd2012 <lyd20122024@outlook.com>
2024-10-20 15:10:43 +0800

    build(Boot): 将 Boot.c 文件从 C 语言改为 C++ 语言
    - 将 Boot.c 文件重命名为 Boot.cpp
    - 更新 Build.bat 文件中的编译命令，使用 g++ 编译器替代 gcc
    - 调整编译选项以适应 C++ 语言

--- Build.bat
+++ Build.bat
@@ -7,7 +7,7 @@
 @mkdir Boot
 @cd ..
 @cd ..
-@gcc -W -Wno-missing-field-initializer -Wno-unused-parameter -Wno-unused-but-set-variable -Wno-unused-variable -Wextra -e BootLoaderMain -nostdinc -nostdlib -fno-builtin -Wl,--subsystem,10 -o Build\EFI\Boot\BootX64.efi Boot\Boot.c -I Includes
+@g++ -W -Wno-missing-field-initializer -Wno-unused-parameter -Wno-unused-but-set-variable -Wno-unused-variable -Wextra -e BootLoaderMain -nostdinc -nostdlib -fno-builtin -Wl,--subsystem,10 -o Build\EFI\Boot\BootX64.efi Boot\Boot.cpp -I Includes
 @x86_64-elf-g++.exe -O3 -Wall -Wno-unused-parameter -Wno-unused-but-set-variable -Wno-unused-variable -ffreestanding -fno-exceptions -fno-rtti -std=c++23 -c Kernel\Kernel.cpp -o Build\Kernel.o -I Includes
 @x86_64-elf-ld.exe -e KernelMain  -z norelro --static -o Build\Kernel.elf Build\Kernel.o -z norelro
 @qemu-system-x86_64 -bios OVMF.fd -m 1024 -net none -drive file=fat:rw:Build,index=0,format=vvfat -serial file:Run.log -device usb-ehci,id=usb -device usb-tablet,bus=usb.0 -device usb-kbd,bus=usb.0
