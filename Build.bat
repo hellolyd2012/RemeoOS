25bde2ababc027fd924c9a5c204d8a857c5b7a02
hellolyd2012 <lyd20122024@outlook.com>
2024-10-20 14:49:55 +0800

    build: 修改 QEMU 启动参数并重定向日志输出
    - 重定向 QEMU 日志输出到 Build\Run.log 文件
    - 移除多余的 -serial 参数

--- Build.bat
+++ Build.bat
@@ -4,5 +4,5 @@
 @g++ -W -Wno-missing-field-initializer -Wno-unused-parameter -Wno-unused-but-set-variable -Wno-unused-variable -Wextra -e BootLoaderMain -nostdinc -nostdlib -fno-builtin -Wl,--subsystem,10 -o Build\BootX64.efi Boot\Boot.cpp -I Includes
 @g++ -O3 -Wall -Wno-unused-parameter -Wno-unused-but-set-variable -Wno-unused-variable -ffreestanding -fno-exceptions -fno-rtti -std=c++23 -c Kernel\Kernel.cpp -o Build\Kernel.o -I Includes
 ld -e KernelMain --static -o Build\kernel.elf Build\Kernel.o
-qemu-system-x86_64 -bios OVMF.fd -m 1024 -net none -drive file=fat:rw:Build,index=0,format=vvfat -serial file:serial.log -device usb-ehci,id=usb -device usb-tablet,bus=usb.0 -device usb-kbd,bus=usb.0
+qemu-system-x86_64 -bios OVMF.fd -m 1024 -net none -drive file=fat:rw:Build,index=0,format=vvfat -serial file:Build\Run.log -device usb-ehci,id=usb -device usb-tablet,bus=usb.0 -device usb-kbd,bus=usb.0
 @echo on
