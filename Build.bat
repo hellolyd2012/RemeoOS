871b8d4d561d66e1f84905c5e5008039c1702123
hellolyd2012 <lyd20122024@outlook.com>
2024-10-20 14:59:32 +0800

    build(Boot): 更新 BootX64.efi 的生成和加载路径
    - 修改生成路径为 Build\EFI\Boot\BootX64.efi
    - 更新 QEMU 加载路径为 Build 目录下的虚拟磁盘
    - 优化链接器选项，指定 ELF 格式
    - 调整 QEMU 启动参数，统一日志输出位置

--- Build.bat
+++ Build.bat
@@ -1,8 +1,14 @@
 @echo off
 @title RemeoOS编译
 @mkdir Build
-@g++ -W -Wno-missing-field-initializer -Wno-unused-parameter -Wno-unused-but-set-variable -Wno-unused-variable -Wextra -e BootLoaderMain -nostdinc -nostdlib -fno-builtin -Wl,--subsystem,10 -o Build\BootX64.efi Boot\Boot.cpp -I Includes
+@cd Build
+@mkdir EFi
+@cd EFI
+@mkdir Boot
+@cd ..
+@cd ..
+@g++ -W -Wno-missing-field-initializer -Wno-unused-parameter -Wno-unused-but-set-variable -Wno-unused-variable -Wextra -e BootLoaderMain -nostdinc -nostdlib -fno-builtin -Wl,--subsystem,10 -o Build\EFI\Boot\BootX64.efi Boot\Boot.cpp -I Includes
 @g++ -O3 -Wall -Wno-unused-parameter -Wno-unused-but-set-variable -Wno-unused-variable -ffreestanding -fno-exceptions -fno-rtti -std=c++23 -c Kernel\Kernel.cpp -o Build\Kernel.o -I Includes
-ld -e KernelMain --static -o Build\kernel.elf Build\Kernel.o
-qemu-system-x86_64 -bios OVMF.fd -m 1024 -net none -drive file=fat:rw:Build,index=0,format=vvfat -serial file:Build\Run.log -device usb-ehci,id=usb -device usb-tablet,bus=usb.0 -device usb-kbd,bus=usb.0
+ld -f elf -e KernelMain --static -o Build\Kernel.elf Build\Kernel.o
+qemu-system-x86_64 -bios OVMF.fd -m 1024 -net none -drive file=fat:rw:Build,index=0,format=vvfat -serial file:Run.log -device usb-ehci,id=usb -device usb-tablet,bus=usb.0 -device usb-kbd,bus=usb.0
 @echo on
